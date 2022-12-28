b0b2694ecb90211f4f98023459fc7dfd884d117c
MorzanBelen <108069480+MorzanBelen@users.noreply.github.com>
2022-12-28 10:07:03 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,5 @@
+function cartel(){alert("¡Bienvenidos a éste sitio!")}
+
+
+function sitioOscuro(){document.body.style.backgroundColor="grey";}
+function sitioClaro(){document.body.style.backgroundColor="white";}
